6329f7f5a5c545f2b9635d61ffa44174aa4cee2b
AbdurRahman-junior-in-college <162412449+AbdurRahman-junior-in-college@users.noreply.github.com>
2025-05-24 10:50:41 +0430

    Update route.js

--- app/api/topics/route.js
+++ app/api/topics/route.js
@@ -1,25 +1,25 @@
-import { NextResponse } from "next/server";
-import connectDB from "../../../config/db";
+// import { NextResponse } from "next/server";
+// import connectDB from "../../../config/db";
 
 
-export async function POST(request) {
-  const { title, description } = await request.json();
+// export async function POST(request) {
+//   const { title, description } = await request.json();
 
-  await connectDB();
-  const topic = await Topic.create({ title, description });
-  return NextResponse.json({ message: "topic created", topic });
-}
+//   await connectDB();
+//   const topic = await Topic.create({ title, description });
+//   return NextResponse.json({ message: "topic created", topic });
+// }
 
-export async function GET(){
-    await connectDB();
-    const topics = await Topic.find({});
-    return NextResponse.json({topics})
-}
+// export async function GET(){
+//     await connectDB();
+//     const topics = await Topic.find({});
+//     return NextResponse.json({topics})
+// }
 
-export async function DELETE(request) {
-    const id = request.nextUrl.searchParams.get("id");
-    await connectDB;
-    await Topic.findByIdAndDelete(id);
-    return NextResponse.json({success:true, message: "Topic deleted"})
-}
+// export async function DELETE(request) {
+//     const id = request.nextUrl.searchParams.get("id");
+//     await connectDB;
+//     await Topic.findByIdAndDelete(id);
+//     return NextResponse.json({success:true, message: "Topic deleted"})
+// }
 

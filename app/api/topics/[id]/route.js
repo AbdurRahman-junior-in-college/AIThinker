d1dff1669ff8c024c802f4f4f028d02b0b67dd01
AbdurRahman-junior-in-college <162412449+AbdurRahman-junior-in-college@users.noreply.github.com>
2025-05-24 10:50:19 +0430

    Update route.js

--- app/api/topics/[id]/route.js
+++ app/api/topics/[id]/route.js
@@ -1,19 +1,19 @@
-import { NextResponse } from "next/server";
-import connectDB from "../../../../config/db";
+// import { NextResponse } from "next/server";
+// import connectDB from "../../../../config/db";
 
 
-export async function PUT(request, { params }) {
-  const { id } = params;
+// export async function PUT(request, { params }) {
+//   const { id } = params;
 
-  const { newTitle: title, newDes: description } = await request.json();
-  await connectDB();
-  await Topic.findByIdAndUpdate(id, { title, description });
-  return NextResponse.json({ success: true, message: "Topic updated" });
-}
+//   const { newTitle: title, newDes: description } = await request.json();
+//   await connectDB();
+//   await Topic.findByIdAndUpdate(id, { title, description });
+//   return NextResponse.json({ success: true, message: "Topic updated" });
+// }
 
-export async function GET(request, { params }) {
-  const { id } = params;
-  await connectDB();
-  const topic = Topic.findOne({ _id: id });
-  return NextResponse.json({ topic });
-}
+// export async function GET(request, { params }) {
+//   const { id } = params;
+//   await connectDB();
+//   const topic = Topic.findOne({ _id: id });
+//   return NextResponse.json({ topic });
+// }

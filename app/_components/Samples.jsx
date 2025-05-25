041e3c1b6c9ee62f39be734becce85f97e5ac72c
AbdurRahman-junior-in-college <162412449+AbdurRahman-junior-in-college@users.noreply.github.com>
2025-05-25 14:19:04 +0430

    Update Samples.jsx

--- app/_components/Samples.jsx
+++ app/_components/Samples.jsx
@@ -1,14 +1,14 @@
 import React, { Suspense } from "react";
-import { get_samples } from "../../data_lib/data_services";
+// import { get_samples } from "../../data_lib/data_services";
 import SamplesList from "./SamplesList";
 import SpinnerMini from "./SpinnerMini";
 
-export const revalidate = 3600;
+// export const revalidate = 3600;
 
 // This component will show some samples of generating output
-const Samples = async () => {
+const Samples =  ({samples}) => {
   // const { blogs } = await get_blogs();
-  const { samples } = await get_samples();
+  // const { samples } = await get_samples();
 
   return (
       <SamplesList samples={samples} />

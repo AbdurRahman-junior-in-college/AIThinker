b578c3f09e38284ecd49ccdcb5b8b7a264f801c0
AbdurRahman-junior-in-college <162412449+AbdurRahman-junior-in-college@users.noreply.github.com>
2025-05-25 17:49:07 +0430

    Update page.jsx

--- app/blogs/[userid]/page.jsx
+++ app/blogs/[userid]/page.jsx
@@ -9,56 +9,16 @@ const Blogs = async ({ params }) => {
   const { userid } = await params;
 
   const { blogs } = await get_blogs(userid);
-  // console.log(blogs)
-
-  //   export async function getServerSideProps(context) {
-  //     const {id} = context.params;
-  //     const res = await fetch(`http://localhost:3000/api/blogs/${id}`);
-  //     return res.json();
-  // }
-
-  //   // console.log(id)
-  //   // const { user } = useUser();
-  //   // const user = await currentUser();
-  //   // // const user = await current
-  //   // console.log(user);
-  //   // if (!user) {
-  //   //   return null;
-  //   // }
-  //   // const userId = user?.id;
-
-  //   const hasFetched = useRef(false);
-  //   let blogs = [];
-
-  //   useEffect(() => {
-  //     if (hasFetched.current) return;
-  //     hasFetched.current = true;
-
-  //     async function blogsGetting() {
-  //       const res = await get_blogs(userid);
-  //       blogs = res.blogs;
-  //     }
-  //   blogsGetting();
-
-  //   // const res = blogsGetting();
-  //   // blogs = res;
-  //   // blogsGetting();
-  //   }, []);
-
-  //   // const {blogs} = await get_blogs(userid);
-  //   console.log(blogs)
-  // // let blogs = [];
-  //   if (!blogs) {
-  //     return (
-  //       <div className="text-3xl text-amber-50">
-  //         <h1>No Blogs</h1>
-  //       </div>
-  //     );
-  //   }
   return (
     <div>
       <Suspense fallback={<Spinner />}>
-        <BlogList blogs={blogs} />
+        <>
+          {blogs.length > 0 ? (
+            <BlogList blogs={blogs} />
+          ) : (
+            <h1 className="text-white">Blogs Not found.</h1>
+          )}
+        </>
       </Suspense>
     </div>
   );

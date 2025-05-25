d8493dc963943988695f52871bf05b97eebdcce7
AbdurRahman-junior-in-college <162412449+AbdurRahman-junior-in-college@users.noreply.github.com>
2025-05-25 15:00:03 +0430

    Update RemoveBtn.jsx

--- app/_components/RemoveBtn.jsx
+++ app/_components/RemoveBtn.jsx
@@ -1,30 +1,31 @@
 'use client'
 
 import React from "react";
-import { useRouter } from "next/navigation";
-const RemoveBtn = ({ id }) => {
+// import { useRouter } from "next/navigation";
+const RemoveBtn = () => {
 
-    const router = useRouter()
-  const removeTopic = async () => {
-    const confirmed = confirm("Are you sure to delete");
+  //   const router = useRouter()
+  // const removeTopic = async () => {
+  //   const confirmed = confirm("Are you sure to delete");
 
-    if (confirmed) {
-      const res = await fetch(`http://localhost:3000/api/topics?id=${id}`, {
-        method: "DELETE",
-      });
+  //   if (confirmed) {
+  //     const res = await fetch(`http://localhost:3000/api/topics?id=${id}`, {
+  //       method: "DELETE",
+  //     });
 
-      if(res.ok) {
-        router.refresh();
-      }
-    }
-  };
+  //     if(res.ok) {
+  //       router.refresh();
+  //     }
+  //   }
+  // };
   return (
-    <button
-      onClick={removeTopic}
-      className="font-bold text-green-500 outline-none border-none cursor-pointer"
-    >
-      D
-    </button>
+    // <button
+    //   onClick={removeTopic}
+    //   className="font-bold text-green-500 outline-none border-none cursor-pointer"
+    // >
+    //   D
+    // </button>
+      <h1>Remove</h1>
   );
 };
 
